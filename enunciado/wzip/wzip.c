2ae5c5a2c5f5130a5ea053c2ed739cd7e9954da2
Unknown <unknown@example.com>
2020-10-05 03:18:36 -0500

    wzip done

---
+++ enunciado/wzip/wzip.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+    
+    if(argc == 1){
+        printf("wzip: file1 [file2 ...]\n");
+        return 1;
+    }
+
+    FILE *fp;
+    int count = 1;
+    int aux = 0;
+    char character, oldChar;
+
+    for(int i=1; i<argc; i++){
+      
+      fp = fopen(argv[i],"r");
+
+      if(fp == NULL) {
+        printf("wzip: cannot open file\n");
+        return 1;
+      }
+
+      character = fgetc(fp);
+      
+      while(character != EOF){
+        if(character == oldChar){
+          count = count + 1;
+        }
+        else if(aux != 0){
+          if(character != oldChar){
+            fwrite(&count, 1, sizeof(count), stdout);
+            printf("%c", oldChar);
+            count =  1;
+          }
+        }
+        aux = 1;
+        oldChar = character;
+        character = fgetc(fp);
+      }
+      fclose(fp);
+    }
+  fwrite(&count, 1, sizeof(count), stdout);
+  printf("%c", oldChar);
+  return(0);  
+}
